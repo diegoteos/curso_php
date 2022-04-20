888fc2da403c14d77c0354064c88a26d82187fcd
diegoteos <diegoteos@hotmail.com>
2022-04-19 22:42:29 -0600

    Update imprimir_en.php

--- imprimir_en.php
+++ imprimir_en.php
@@ -1,5 +1,18 @@
 <?php
 
+echo "Para imprimir variables tipo String debemos usar comillas simples ' '<br><br/>";
+
+echo 'Al usar comillas dobles "" php busca en variables para su ejecucion lo cual hara que demore mas tiempo<br><br/>';
+
+echo 'Con "echo" imprimimos cadenas de texto <br />';
+
+
+
+
+
+
+
+
 
 
 ?>
