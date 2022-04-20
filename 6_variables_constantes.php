ccacb8d3cc87f6286db048548687ace50a12fe5b
diegoteos <diegoteos@hotmail.com>
2022-04-19 23:30:07 -0600

    Variables y constantes

---
+++ 6_variables_constantes.php
@@ -0,0 +1,26 @@
+<?php
+
+$edad = 32; // variables
+
+echo $edad."<br/>";
+
+$edad = 40; // cambiarmos el valor de la variable
+
+echo $edad."<br/>";
+
+
+// uso de constante
+
+define("NOMBRE","DIEGO");
+
+// imprimir constante
+
+echo NOMBRE."<br/>";
+
+
+define("APELLIDO","PENADO"); //BUENA PRACTICA ESCRIBIR LAS COSNTANTES EN MAYUSCULA
+
+echo APELLIDO."<br/>";
+
+
+?>
