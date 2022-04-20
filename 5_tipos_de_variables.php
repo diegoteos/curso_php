7c005da912ddde043f51cf9ec5d0d4487eae0a53
diegoteos <diegoteos@hotmail.com>
2022-04-19 23:26:37 -0600

    Tipos de variables y concatenacion

---
+++ 5_tipos_de_variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$edad = 32; // entero
+
+$nombre = "oscar"; // string
+
+$altura = 1.4; // Flotante
+
+$programador = true; // Valor nulo
+
+echo "El nombre es ".$nombre." la edad es  ".$edad." su altura es ".$altura ." y el puesto es ".$programador;
+
+?>
