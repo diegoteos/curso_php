9e9e04204b7b128d248eb57a22ad816d5a02e3e3
diegoteos <diegoteos@hotmail.com>
2022-04-21 14:42:00 -0600

    arreglos con indice no numerico

--- 23_arreglos_asociativos.php
+++ 23_arreglos_asociativos.php
@@ -1,5 +1,10 @@
 <?php
 
-    
+
+    # DEFINIMOS LOS VALORES CON EL INDICE QUE NOSOTROS QUERAMOS
+    $frutas = array("p"=>"pera", "m"=>"manzana", "f"=>"fresa", "l"=>"licha");
+
+    # VEMOS EL INDICE Y SUS VALORES
+    print_r($frutas);
 
 ?>
