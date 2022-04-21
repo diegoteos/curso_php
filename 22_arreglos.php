9877c070b04c6e7a34a9b3e2c39b226476991130
diegoteos <diegoteos@hotmail.com>
2022-04-21 14:30:58 -0600

    arreglos

--- 22_arreglos.php
+++ 22_arreglos.php
@@ -7,7 +7,27 @@
     # funcion print_r nos muestra como esta organizada la 
     # informacion segun su indice empezando con la posicion 0
 
+
+    #vemos los valores del array con su indice
+    echo 'forma 1<br />';
     print_r($frutas);
+
+    # forma de ver los valores con print_r
+    echo '<br><br><br />';
+    echo 'forma 2';
+    echo('<pre>');
+    print_r($frutas);
+    echo('</pre>');
+    
+
+
+    # forma de ver los valores con var_dump
+    echo '<br>forma 3<br />';
+    echo('<pre>');
+    var_dump($frutas);
+    echo('</pre>');
+
+
 
 
 
