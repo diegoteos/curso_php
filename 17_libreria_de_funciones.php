3e3e2f9a96a24b6da15efc32a51658671a83610e
diegoteos <diegoteos@hotmail.com>
2022-04-20 22:50:04 -0600

    Libreria de funciones PHP

--- 17_libreria_de_funciones.php
+++ 17_libreria_de_funciones.php
@@ -1,12 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librerias de funciones PHP</title>
+</head>
+<body>
+    <center><h3>Libreria de funciones de php</h3>
 
-LIBRERIA DE FUNCIONES YA CREADAS EN PHP 
+    <a href="https://www.php.net/manual/es/function.hash-init.php">LIBRERIA DE FUNCIONES YA CREADAS EN PHP</a><br><br>
 
-https://www.php.net/manual/es/function.hash-init.php
+    <a href="https://www.php.net/manual/es/ref.math.php">FUNCIONES MATEMATICAS:</a></center>
+
+</body>
+</html>
 
 
-
-
-
-FUNCIONES MATEMATICAS:
-
-https://www.php.net/manual/es/ref.math.php
