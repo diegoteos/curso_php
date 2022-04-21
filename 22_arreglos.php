b4075718ee2a5910d799e34f66b45937244ed77b
diegoteos <diegoteos@hotmail.com>
2022-04-21 14:15:09 -0600

    arreglos de indice numerico

---
+++ 22_arreglos.php
@@ -0,0 +1,14 @@
+<?php
+
+    # los arreglos son un conjunto de datos 
+    # array es una funcion con un arreglo o conjunto de datos 
+    $frutas = array("manzana", "fresa", "pera");
+
+    # funcion print_r nos muestra como esta organizada la 
+    # informacion segun su indice empezando con la posicion 0
+
+    print_r($frutas);
+
+
+
+?>
