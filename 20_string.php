bca4e974d091d85d8f9c07a3c36e2920b8bb0974
diegoteos <diegoteos@hotmail.com>
2022-04-21 12:41:44 -0600

    funciones para string de mayusculas a minusculas

---
+++ 20_string.php
@@ -0,0 +1,17 @@
+<?php
+    # CREAMOS UNA VARIABLE TIPO STRING
+    $miNombre = 'diego teos fue el que escribio este codigo';
+
+    # CON STRTOUPPER CAMBIAMOS EL STRING EN MAYUSCULA
+    $mayuscula = strtoupper($miNombre);
+    echo $mayuscula.'<br><br />';
+
+    # AHORA CAMBIAMOS A MINUSCULAS EL STRING ANTERIOR USANDO STRTOLOWER 
+    $minuscula = strtolower($mayuscula);
+    echo $minuscula;
+
+
+
+
+?>
+
