5079c15b22f62c0038a4034c1f7daa5e26780834
diegoteos <diegoteos@hotmail.com>
2022-04-21 23:31:38 -0600

    numero random y cifrado con md5

--- 18_funcion_num_rand.php
+++ 18_funcion_num_rand.php
@@ -1,16 +1,21 @@
 <?php
 
-//$numero = rand(4, 2);
-//echo $numero;
+$numero = rand(4, 2);
+echo 'Numero random: '.$numero.'<br><br/>';
 
+
+echo "Aca estamos usando md5 para cifrar el string hola <br/>";
 $h1 = hash('md5', 'hola');
 
-echo $h1.'<br/>';
+echo $h1.'<br><br/>';
 
+echo "Aca estamos usando md5 para cifrar el hash generado del string hola, <br/>";
 $h2 = hash('md5', $h1);
+echo $h2.'<br/>';
 
 echo '<br><br>';
 
+echo "Aca estamos estamos uniendo los dos valores hash por simple locura mia, como combinacion para crear un algoritmo de cifrado un poco mas seguro<br/>";
 echo $h1.$h2;
 
 ?>
