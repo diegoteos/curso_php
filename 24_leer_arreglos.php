8f483728bcf347c41706e131ba036834f2e2a03d
diegoteos <diegoteos@hotmail.com>
2022-04-22 23:43:41 -0600

    uso de array_push para modificar arreglos

--- 24_leer_arreglos.php
+++ 24_leer_arreglos.php
@@ -1,10 +1,11 @@
 <?php
 
-    $frutas= array("fresa", "pera", "manzana", "toronja");
+    $frutas= array("Fresa", "Pera", "Manzana", "Toronja");
+
+
+    # ARRAY_PUSH ES UNA FUNCION QUE AYUDA A MODIFICAR LOS ARREGLOS
+    array_push($frutas, "Uva"); 
 
     print_r($frutas);
-
-    echo '<br><br />';
-    echo $frutas[2];
 
 ?>
