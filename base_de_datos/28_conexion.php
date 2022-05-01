3e688ddea919ecfc27a7ca7d468a3196c8954638
diegoteos <diegoteos@hotmail.com>
2022-04-30 23:07:58 -0600

    conexion e insercion a la base de datos

--- base_de_datos/28_conexion.php
+++ base_de_datos/28_conexion.php
@@ -7,6 +7,11 @@ $contraseña = "";
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=album", $usuario, $contraseña);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'Jugnado con la programacion', 'foto.jpg');";
+
+    $conexion->exec($sql);
+
     echo "Conexion establecida.";
 } catch (PDOException $error) {
     echo "Conexion erronea" . $error;
