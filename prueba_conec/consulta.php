b66fe6209c35b59ec1d09eeee96b54121d446507
diegoteos <diegoteos@hotmail.com>
2022-05-08 22:26:38 -0600

    consultas a DB

--- prueba_conec/consulta.php
+++ prueba_conec/consulta.php
@@ -1,36 +1,67 @@
 <?php
 
-if (isset($_POST['telefono'])){
+if (isset($_POST['telefono']) && empty($_POST['dpi'])) {
     $telefono = $_POST['telefono'];
-}else {
-    echo "Debe de ingresar un numero de telefono para consultar en la db";
-}
 
-$servidor = "localhost"; // 127.0.0.1
-$usuario = "root";
-$contraseña = "";
 
-try {
-    $conexion = new PDO("mysql:host=$servidor;dbname=expertajes", $usuario, $contraseña);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $servidor = "localhost"; // 127.0.0.1
+    $usuario = "root";
+    $contraseña = "";
 
-    $sql = "SELECT * FROM `exp` WHERE `COL 17`= $telefono;";
+    try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=expertajes", $usuario, $contraseña);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //$sql = "SELECT * FROM `01_30_noviembre_2020`";
+        $sql = "SELECT * FROM `exp` WHERE `COL 17`= $telefono;";
 
-    $sentencia = $conexion->prepare($sql);
-    $sentencia->execute();
+        //$sql = "SELECT * FROM `01_30_noviembre_2020`";
 
-    $resultado = $sentencia->fetchAll();
+        $sentencia = $conexion->prepare($sql);
+        $sentencia->execute();
 
-   // print_r($resultado);
+        $resultado = $sentencia->fetchAll();
 
-    foreach($resultado as $abonado) {
-        echo 'Nombres: ' . $abonado['COL 8']."<br/> Apellidos: ".$abonado['COL 9'] ." ". ' <br/>DPI: '.$abonado['COL 10']."<br> Nit:".$abonado['11']."<br> Correo:".$abonado['13'] .'<br><br><br/>';
+        foreach ($resultado as $abonado) {
+            echo 'Nombres: ' . $abonado['COL 8'] . "<br/> Apellidos: " . $abonado['COL 9'] . " " . ' <br/>DPI: ' . $abonado['COL 10'] . "<br> Nit:" . $abonado['11'] . "<br> Correo:" . $abonado['13'] . "<br> Departamento:" . $abonado['17'] .  "<br> Municipio:" . $abonado['18'] .  "<br> Dirección:" . $abonado['19'] . '<br><br><br/>';
+        }
+
+
+        echo "Conexion establecida.";
+    } catch (PDOException $error) {
+        echo "Conexion erronea" . $error;
     }
-    
 
-    echo "Conexion establecida.";
-} catch (PDOException $error) {
-    echo "Conexion erronea" . $error;
+} elseif (isset($_POST['dpi']) && empty($_POST['telefono'])) {
+    $dpi = $_POST['dpi'];
+
+
+    $servidor = "localhost"; // 127.0.0.1
+    $usuario = "root";
+    $contraseña = "";
+
+    try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=expertajes", $usuario, $contraseña);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT * FROM `exp` WHERE `COL 10`= $dpi;";
+
+        $sentencia = $conexion->prepare($sql);
+        $sentencia->execute();
+
+        $resultado = $sentencia->fetchAll();
+
+        foreach ($resultado as $persona) {
+            echo 'Nombres: ' . $persona['COL 8'] . "<br/> Apellidos: " . $persona['COL 9'] . " " . ' <br/>DPI: ' . $persona['COL 10'] . "<br> Nit:" . $persona['11'] . "<br> Correo:" . $persona['13'] . "<br> Departamento:" . $persona['17'] .  "<br> Municipio:" . $persona['18'] .  "<br> Dirección:" . $persona['19'] . '<br><br><br/>';
+        }
+
+
+        echo "Conexion establecida.";
+    } catch (PDOException $error) {
+        echo "Conexion erronea" . $error;
+    }
+} elseif (isset($_POST['dpi']) && !empty($_POST['telefono']) || isset($_POST['telefono']) && !empty($_POST['dpi'])) {
+    echo 'Solo puede hacer una consulta, o de telefono o de DPI.';
+}
+else {
+    echo "Debe de ingresar una consulta para que se le muestre algun resultado.";
 }
