663c832e266ac345dc949f4649e04849d4cbf52f
diegoteos <diegoteos@hotmail.com>
2022-05-27 22:46:16 -0600

    Portafolio

--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -1,8 +1,59 @@
 <?php
 include_once 'cabecera.php';
 ?>
+<br />
 
-Hola, bienvenido al portafolio
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Datos del proyecto
+                </div>
+                <div class="card-body">
+                    <form action="portafolio.php" method="post">
+                        Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
+                        <br>
+                        Imagen del proyecto: <input class="form-control" type="file" name="archivo" id="">
+                        <br>
+                        <input class="btn btn-success" type="submit" value="Enviar proyecto">
+                    </form>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>aplicacion Web</td>
+                        <td>imagen.jpg</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
 
 
 <?php
