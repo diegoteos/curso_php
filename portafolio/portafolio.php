24a764452b8096939ae3145e6ea357d0e28aebad
diegoteos <diegoteos@hotmail.com>
2022-05-31 20:44:15 -0600

    protafolio PHP

--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -1,6 +1,33 @@
 <?php
 include_once 'cabecera.php';
+include_once 'conexion.php';
 ?>
+<?php
+
+if ($_POST) {
+   // print_r($_POST);
+    $nombre = $_POST['nombre'];
+    $objConexion = new conexion();
+    $sql = "INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', 'imagen.jpg', 'esta es la primera imagen');";
+    $objConexion->ejecutar($sql);
+}
+
+
+if($_GET){
+    $id = $_GET['borrar'];
+    $objConexion = new conexion();
+    $sql = "DELETE FROM proyectos WHERE `proyectos`.`id` = '$id'";
+    $objConexion->ejecutar($sql);
+    
+
+}
+$objConexion = new conexion();
+$proyectos = $objConexion->consultar("SELECT * FROM `proyectos`");
+
+
+?>
+
+
 <br />
 
 <div class="container">
@@ -11,7 +38,7 @@ include_once 'cabecera.php';
                     Datos del proyecto
                 </div>
                 <div class="card-body">
-                    <form action="portafolio.php" method="post">
+                    <form action="portafolio.php" method="post" enctype="multipart/form-data">
                         Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
                         <br>
                         Imagen del proyecto: <input class="form-control" type="file" name="archivo" id="">
@@ -29,19 +56,21 @@ include_once 'cabecera.php';
                         <th>ID</th>
                         <th>Nombre</th>
                         <th>Imagen</th>
+                        <th>Descripcion</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
+
+                <?php foreach ($proyectos as $proyecto) { ?>
                     <tr>
-                        <td scope="row"></td>
-                        <td></td>
-                        <td></td>
+                    <td><?php echo $proyecto['id']; ?></td>
+                    <td><?php echo $proyecto['nombre']; ?></td>
+                    <td><?php echo $proyecto['imagen']; ?></td>
+                    <td><?php echo $proyecto['descripcion']; ?></td>
+                    <td><a class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>">Eliminar</a></td>
                     </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>aplicacion Web</td>
-                        <td>imagen.jpg</td>
-                    </tr>
+                <?php } ?> 
                 </tbody>
             </table>
 
